9168634208775a229ab02d6467a2b9438d083f8e
Awet Tesfay <whitezscope@gmail.com>
2020-09-14 14:56:27 +0200

    interchange some codes

--- index.js
+++ index.js
@@ -12,7 +12,24 @@ document.querySelector("#search-btn").addEventListener("click", ()=> {
                 console.log(myJson);
                 document.querySelector("#pokémon-id").innerHTML = `${myJson.id}`;
                 document.querySelector("#pokémon-img").setAttribute("src", `${myJson.sprites.front_shiny}`);
-
+                let moveUrl = myJson.moves[0].move.url;
+                fetch(`${moveUrl}`)
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((myJson) => {
+                        console.log(myJson);
+                        let moveOne = myJson.contest_effect.url;
+                        let moveTwo = myJson.contest_effect.url;
+                        fetch(`${moveOne}`)
+                            .then((response) => {
+                                return response.json();
+                            })
+                            .then((myJson) => {
+                                console.log(myJson);
+                                document.getElementById("move").innerHTML = `${myJson.effect_entries[0].effect}`
+                            });
+                });
         }).catch(error => {
             console.log(error);
         })
