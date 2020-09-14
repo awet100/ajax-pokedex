d495b7966fa766ab8612ccb8668640d28313935e
Awet Tesfay <whitezscope@gmail.com>
2020-09-14 14:05:55 +0200

    get the data from pokemon api and link it to the page

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // listen to the click event of the search btn
 document.querySelector("#search-btn").addEventListener("click", ()=> {
-    // search by id or name
-    let inputValue = document.querySelector("#search-input");
-    let inputArray = inputValue.split(" ");
-    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-        .then((response) => {
-            return response.json();
+    // search pokemon by id or name
+    let inputValue = document.querySelector("#search-input").value;
+
+    // get data from remote server to the corresponding input
+    if (true) {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`)
+            .then((response) => {
+                return response.json();
+            }).then((myJson) => {
+                console.log(myJson);
+                document.querySelector("#pokémon-id").innerHTML = `${myJson.id}`;
+                document.querySelector("#pokémon-img").setAttribute("src", `${myJson.sprites.front_shiny}`);
+
+        }).catch(error => {
+            console.log(error);
         })
-        .then((myJson) => {
-            console.log(myJson);
-        });
+    }
 })
