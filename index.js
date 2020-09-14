ab0da3ba3897e89246267d629502da550dc1f9f5
Awet Tesfay <whitezscope@gmail.com>
2020-09-14 11:40:06 +0200

    use fetch to get the api

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-    .then(response => {
-        return response.json();
-    }).then(data => {
-    console.log(data);
+// listen to the click event of the search btn
+document.querySelector("#search-btn").addEventListener("click", ()=> {
+    // search by id or name
+    let inputValue = document.querySelector("#search-input");
+    let inputArray = inputValue.split(" ");
+    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
 })
