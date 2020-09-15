1ba4cb441c539c100d835c0841df16a440d09463
Awet Tesfay <whitezscope@gmail.com>
2020-09-15 15:14:15 +0200

    get the previos evolution of pakeman

--- index.js
+++ index.js
@@ -4,19 +4,16 @@ document.querySelector("#search-btn").addEventListener("click", ()=> {
     let inputValue = document.querySelector("#search-input").value;
 
     // get data from remote server to the corresponding input
-    if (true) {
+    if (inputValue) {
         fetch("https://pokeapi.co/api/v2/pokemon/"+inputValue)
             .then((response) => {
                 return response.json();
             }).then((myData) => {
-                console.log(myData); // test
-                document.getElementById("pokémon-id").innerHTML = myData.id;
+                document.getElementById("pokémon-id").innerHTML = "Pokeman's Id: " + myData.id;
                 document.getElementById("pokémon-img").setAttribute("src", myData.sprites.front_shiny);
                 return myData.moves;
             }).then(movesData => {
-                console.log(movesData); // test
                 let fourMovesObject = movesData.slice(0, 4);
-                console.log(fourMovesObject); // test
             for (let i = 0; i < fourMovesObject.length; i++) {
                 fetch(fourMovesObject[i].move.url)
                     .then(respond => {
@@ -28,6 +25,24 @@ document.querySelector("#search-btn").addEventListener("click", ()=> {
 
         })
         // new fetch
-
+        fetch("https://pokeapi.co/api/v2/pokemon/"+inputValue)
+            .then(respond => {
+            return respond.json();
+        }).then(myData => {
+            return myData.species.url;
+        }).then(evolutionUrl => {
+            fetch(evolutionUrl)
+                .then(respond => {
+                    return respond.json();
+            }).then(myData => {
+                return myData.evolves_from_species.url;
+            }).then(evolutionFromUrl => {
+                fetch(evolutionFromUrl).then(evolutionDate => {
+                    return evolutionDate.json();
+                }).then(data => {
+                    console.log(data);
+                })
+            })
+        })
     }
 })
